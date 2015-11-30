3553a617370c3a1d63123205c78fd162b9de509f
ezhu <ezhu@ubuntu.local>
2015-11-30 04:55:45 -0800

    Fixing docker credential files

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -1,20 +1,10 @@
 #!/bin/bash -xe
 
-# move out exiting content
-mkdir -p /tmp/s3ql
-sudo mv $MOUNT_DIR/* /tmp/s3ql
-
 # mount s3ql file system
 sudo /bin/s3ql.sh mount \
      $MOUNT_DIR $CACHE_DIR \
      $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY \
      $FS_PATH $FS_LABEL $FS_PASSPHRASE
-
-# copy back existing content
-for dirent in /tmp/s3ql/*; do
-    sudo cp -a $dirent $MOUNT_DIR
-done
-sudo rm -rf /tmp/s3ql
 
 # install periodic snapshot job to minimize data loss
 sudo /bin/s3ql.sh install $MOUNT_DIR $(basename $JENKINS_HOME)
