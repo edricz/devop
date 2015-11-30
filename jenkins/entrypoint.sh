b1ee7a2401aa72024eb883c279a4afd6207f7c91
ezhu <ezhu@ubuntu.local>
2015-11-29 21:29:11 -0800

    Fixing bug in s3ql mount

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -2,15 +2,18 @@
 
 # move out exiting content
 mkdir -p /tmp/home
-sudo mv $JENKINS_HOME/* /tmp/home
-export MOUNT_DIR=$JENKINS_HOME
+sudo mv $JENKINS_HOME /tmp/home
+sudo mkdir -p $JENKINS_HOME
 
 # mount s3ql file system
-/bin/s3ql.sh mount
+sudo /bin/s3ql.sh mount \
+     $JENKINS_HOME $CACHE_DIR \
+     $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY \
+     $FS_PATH $FS_LABEL $FS_PASSPHRASE
 
 # copy back existing content
 for dirent in /tmp/home/*; do
-    sudo cp -a $dirent $JENKINS_HOME
+    sudo cp -a $dirent $(dirname $JENKINS_HOME)
 done
 sudo rm -rf /tmp/home
 
